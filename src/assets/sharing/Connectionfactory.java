c14606f6df0e9471cc831af01dcff4657b7bacac
giiannace56 <giovanniiannace56@gmail.com>
2023-05-10 22:00:43 -0300

    bd-factory

--- src/assets/sharing/Connectionfactory.java
+++ src/assets/sharing/Connectionfactory.java
@@ -6,12 +6,13 @@ import javax.swing.JOptionPane;
 
 public class Connectionfactory {
 
-    public Connection Connectionfactory (){
+    public Connection Connectionfactory(){
             Connection conn = null;
-
+            System.out.println("qqlc");
             try{
-                String url = "jdbc:mysql://localhost:3306/" ; //ainda falta nome do banco user e password;
+                String url = "jdbc:mysql://localhost:3306/estudante_2sem_cc?user=root&password="; //ainda falta nome do banco user e password;
                 conn = DriverManager.getConnection(url);
+                System.out.println(conn);
             } catch (SQLException erro){
                 JOptionPane.showMessageDialog(null, erro.getMessage());
             }
